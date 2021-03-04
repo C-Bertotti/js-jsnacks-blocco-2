b0676e6c48de43cff475c4c1e12c000e7a2d0ead
Caterina Bertotti <bertotti4997@gmail.com>
2021-03-04 12:01:26 +0100

    fix snack 1

--- js-snack-1/js/script.js
+++ js-snack-1/js/script.js
@@ -4,31 +4,35 @@ inseriti. Esegui questo programma in due versioni, con il for
 e con il while. */
 
 // Con ciclo for
-/* var numbers = [];
-
+/* 
 var sum = 0;
 
 for (var i = 0; i < 5; i++) {
-    var userNumber = parseInt(prompt('Inserisci un numero'));
+    do {
+        var userNumber = parseInt(prompt('Inserisci un numero'));
+    } while ( isNan(userNumber) );
+    
     console.log(userNumber);
-    numbers.push(userNumber);
-    sum += numbers[i];
+   
+    sum += userNumber;
 }
 
 console.log(sum);
 */
 
 // Con ciclo while
-var numbers = [];
 
 var sum = 0;
+var userNumber;
 
 var i = 0;
 while (i < 5) {
-    var userNumber = parseInt(prompt('Inserisci un numero'));
+    do {
+        userNumber = parseInt(prompt('Inserisci un numero'));
+    } while ( isNaN(userNumber) );
+    
     console.log(userNumber);
-    numbers.push(userNumber);
-    sum += numbers[i];
+    sum += userNumber;
     i++;
 }
 
